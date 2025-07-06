287f9dc00ab3c51facdd15cda5348a2352cfc265
Stream5937 <149094480+Stream5937@users.noreply.github.com>
2025-07-06 15:16:10 +0100

    add clearAll functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,34 @@ import { useState } from 'react'
 import './App.css'
 import Page_Container from './components/pageContainer.jsx'
 
+
+
 function App() {
   
   const [clearData, setClearData] = useState(false);
 
+  function f_clearData () {
+    setClearData(true); 
+    window.location.reload(false);
+  }
+
   return (
-    
-    <>
-    
+    <> 
     <div className='outer_container'>
       <div className='container'>
-        <Page_Container />
+        <Page_Container 
+          clearForm= {clearData}/>
         <div className="card">
-          <button onClick={() => setClearData((clearData) => alert('confirm clear all input data'))}>
+          <button onClick={() => f_clearData()}>
             Clear all
           </button>
         </div>
       </div>
     </div>
-    
-    
     </>
-    
   )
 
 }
 
 export default App
+
--- src/components/pageContainer.jsx
+++ src/components/pageContainer.jsx
@@ -7,7 +7,8 @@ import RenderField from './RenderField.jsx'
 import '../styles/page_container.css'
 import editIcon from '../assets/edit-1.png'
 
-function Page_Container () {
+function Page_Container (props) {
+
 
     const [form_gen, setFormGen] = useState('');
     const [form_edu, setFormEdu] = useState('');
@@ -46,8 +47,17 @@ function Page_Container () {
         setEditFormExp(false);
   };
 
+  if(props.clearForm){
+      console.log('clearForm -true: ', props.clearForm)
+      //saveInputValue('');
+      //saveInputValueEdu('');
+      //saveInputValueExp('');
+    }else{
+      console.log('clearForm -false: ', props.clearForm)
+    }
+
   return (
-        <>
+      <>
         <div className="basic-form-fill page_container">
            {!editFormGen && (
               <MyForm_gen 
@@ -56,14 +66,21 @@ function Page_Container () {
                 infoToEdit={form_gen}
               />
             )}
-            {editFormGen && (
+            {editFormGen && !props.clearForm &&(
               <MyForm_gen 
                 saveInputValue={saveInputValue}
                 isEditing={editFormGen}
                 infoToEdit={form_gen}
               />
             )}
-            {form_gen != '' && (
+            {editFormGen && props.clearForm && (
+              <MyForm_gen 
+                saveInputValue={saveInputValue ('')}
+                isEditing={editFormGen}
+                infoToEdit={form_gen}
+              />
+            )}
+            {form_gen != '' && !props.clearForm && (
               <div className="basic-form-show">
                 <div className="basic-form-general-info">
                   <RenderField props={form_gen} />
@@ -77,15 +94,159 @@ function Page_Container () {
                 </div>
               </div>
             )}
-          </div>
+        </div>
+        {!props.clearForm && (
             <div className="cv-show">
-              <div className="cv-section" id="cv-report">
-              <div className="cv-basic-info">
-                <RenderField props={form_gen}/>
-              </div>
+                  <div className="cv-section" id="cv-report">
+                  <div className="cv-basic-info">
+                    <RenderField props={form_gen}/>
+                  </div>
+                </div>
             </div>
-          </div>
+          )
+        }
+        {props.clearForm && (
+            <div className="cv-show">
+                  <div className="cv-section" id="cv-report">
+                  <div className="cv-basic-info">
+                    <RenderField props={''}/>
+                  </div>
+                </div>
+            </div>
+          )
+        }
+{//---
+}
+<div className="edu-form-fill page_container">
+           {!editFormEdu && (
+              <MyForm_edu 
+                saveInputValue={saveInputValueEdu}
+                isEditing={editFormEdu}
+                infoToEdit={form_edu}
+              />
+            )}
+            {editFormEdu && !props.clearForm &&(
+              <MyForm_edu 
+                saveInputValue={saveInputValueEdu}
+                isEditing={editFormEdu}
+                infoToEdit={form_edu}
+              />
+            )}
+            {editFormEdu && props.clearForm &&(
+              <MyForm_edu 
+                saveInputValue={saveInputValueEdu('')}
+                isEditing={editFormEdu}
+                infoToEdit={form_edu}
+              />
+            )}
+            {form_edu != '' && !props.clearForm && (
+              <div className="edu-form-show">
+                <div className="edu-form-education-info">
+                  <RenderField props={form_edu} />
+                </div>
+                <div>
+                <img
+                      onClick={editForm_edu}
+                      src={editIcon}
+                      className="edit-form"
+                  />
+                </div>
+              </div>
+            )}
+        </div>
+        {!props.clearForm && (
+		<div className="cv-show">
+		    <div className="cv-section" id="cv-report">
+		      <div className="cv-edu-info">
+		        <RenderField props={form_edu}/>
+		      </div>
+		    </div>
+		</div>
+	    )
+        }
+        {props.clearForm && (
+		<div className="cv-show">
+		    <div className="cv-section" id="cv-report">
+		      <div className="cv-edu-info">
+		        <RenderField props={form_edu}/>
+		      </div>
+		    </div>
+		</div>
+	    )
+        }
+{//---
+  }
 
+{//@@@@@@@@@@@
+  }
+        <div className="exp-form-fill page_container">
+           {!editFormExp && (
+              <MyForm_exp 
+                saveInputValue={saveInputValueExp}
+                isEditing={editFormExp}
+                infoToEdit={form_exp}
+              />
+            )}
+            {editFormExp && !props.clearForm &&(
+              <MyForm_exp 
+                saveInputValue={saveInputValueExp}
+                isEditing={editFormExp}
+                infoToEdit={form_exp}
+              />
+            )}
+            {editFormExp && props.clearForm &&(
+              <MyForm_exp 
+                saveInputValue={saveInputValueExp('')}
+                isEditing={editFormExp}
+                infoToEdit={form_exp}
+              />
+            )}
+            {form_exp != '' && !props.clearForm && (
+              <div className="exp-form-show">
+                <div className="exp-form-education-info">
+                  <RenderField props={form_exp} />
+                </div>
+                <div>
+                <img
+                      onClick={editForm_exp}
+                      src={editIcon}
+                      className="edit-form"
+                  />
+                </div>
+              </div>
+            )}
+        </div>
+        {!props.clearForm && (
+		<div className="cv-show">
+		    <div className="cv-section" id="cv-report">
+		      <div className="cv-exp-info">
+		        <RenderField props={form_exp}/>
+		      </div>
+		    </div>
+		</div>
+	    )
+        }
+        {props.clearForm && (
+		<div className="cv-show">
+		    <div className="cv-section" id="cv-report">
+		      <div className="cv-exp-info">
+		        <RenderField props={form_exp}/>
+		      </div>
+		    </div>
+		</div>
+	    )
+        }
+
+{//@@@@@@@@@@@@@
+}
+      </>
+    )  
+}
+
+export default  Page_Container ;
+
+/*
+//###########
         <div className="edu-form-fill page_container">
            {!editFormEdu && (
               <MyForm_edu 
@@ -115,17 +276,19 @@ function Page_Container () {
                 </div>
               </div>
             )}
-          </div>
-            <div className="cv-show">
-              <div className="cv-section" id="cv-report">
+        </div>
+        <div className="cv-show">
+            <div className="cv-section" id="cv-report">
               <div className="cv-edu-info">
                 <RenderField props={form_edu}/>
               </div>
             </div>
-          </div>
+        </div>
+//#############
 
-
-          <div className="exp-form-fill page_container">
+{//@@@@@@@@@@@
+  }
+        <div className="exp-form-fill page_container">
            {!editFormExp && (
               <MyForm_exp 
                 saveInputValue={saveInputValueExp}
@@ -154,16 +317,14 @@ function Page_Container () {
                 </div>
               </div>
             )}
-          </div>
-            <div className="cv-show">
-              <div className="cv-section" id="cv-report">
+        </div>
+        <div className="cv-show">
+            <div className="cv-section" id="cv-report">
               <div className="cv-edu-info">
                 <RenderField props={form_exp}/>
               </div>
             </div>
-          </div>
-        </>
-    )  
-}
-
-export default  Page_Container ;
+        </div>
+{//@@@@@@@@@@@
+  }
+*/
